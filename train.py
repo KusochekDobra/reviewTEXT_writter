5ca996465772c3f01a4acc784c46aef5bc167add
KusochekDobra <podkidyshev.as@phystech.edu>
2018-04-30 20:15:11 +0300

    Убрал создание спец. файла
    Вот теперь прям красиво

--- train.py
+++ train.py
@@ -17,6 +17,7 @@ import collections
 import argparse
 import os
 import re
+import sys
 
 
 def parse_str(s):
@@ -41,41 +42,34 @@ def file_path_to_good_shape(input_dir):
     return path_f
 
 
-def generate_words(input_dir, out, lc):
+def generate_words(file, lc):
     counter = collections.Counter()
-    for fileName in input_dir:
-        with open(fileName, 'r', encoding="utf8") as file:
-            line = parse_str(file.readline())
+    line = parse_str(file.readline())
 
-            while line:
-                if len(line) > 0:
-                    if lc:
-                        line = ''.join(c for c in line.lower())
+    while line:
+        if len(line) > 0:
+            if lc:
+                line = ''.join(c for c in line.lower())
 
-                allWords = line.split()
-                counter += collections.Counter([' '.join([i for i in allWords]
-                                                         [j:j + 2])
-                                                for j in range(
-                        len(allWords) - 1)])
+        allWords = line.split()
+        counter += collections.Counter([' '.join([i for i in allWords]
+                                                 [j:j + 2])
+                                        for j in range(
+                len(allWords) - 1)])
 
-                last_word = give_last_word(line)
+        last_word = give_last_word(line)
 
-                line = parse_str(file.readline())
-                first_word = give_first_word(line)
+        line = parse_str(file.readline())
+        first_word = give_first_word(line)
 
-                if first_word != '' and last_word != '':
-                    if lc:
-                        last_word = last_word.lower()
-                        first_word = first_word.lower()
+        if first_word != '' and last_word != '':
+            if lc:
+                last_word = last_word.lower()
+                first_word = first_word.lower()
 
-                    counter[parse_str((last_word + ' ' + first_word))] += 1
+            counter[parse_str((last_word + ' ' + first_word))] += 1
 
-    if out != '':
-        with open(out, 'w', encoding="utf8") as output:
-            for i in counter:
-                print('{} {}'.format(i, counter[i]), file=output)
-    else:
-        print('{} {}'.format(i, counter[i]))
+    return counter
 
 
 if __name__ == "__main__":
@@ -90,16 +84,25 @@ if __name__ == "__main__":
                         type=str, help='Дериктория текстов для обучения')
 
     args = parser.parse_args()
-    if args.input_dir == '':
+    counter = collections.Counter()
 
-        with open('additional_input', 'w', encoding="utf8") as add_input:
-            line = input()
-            while line != '':
-                add_input.write(line + '\n')
-                line = input()
-        generate_words(['additional_input'], args.model, args.lc)
+    if args.input_dir == '':
+        # Для того, чтобы оставновить ввод, тыкаейте CTRL + Z
+        counter += generate_words(sys.stdin, args.lc)
     else:
-        generate_words(file_path_to_good_shape(args.input_dir),
-                       args.model, args.lc)
+        for i in file_path_to_good_shape(args.input_dir):
+            print(i)
+            with open(i, 'r', encoding="utf8") as file:
+                counter += generate_words(file, args.lc)
+
+    out = args.model
+
+    if out != '':
+        with open(out, 'w', encoding="utf8") as output:
+            for i in counter:
+                print('{} {}'.format(i, counter[i]), file=output)
+    else:
+        for i in counter:
+            print('{} {}'.format(i, counter[i]))
 
     print(' "{}" generation is completed successfully'.format(args.model))
